11c6fe07e5ecfac35c289e7bc2978789228cd226
hallernsk <avladim@mail.ru>
2024-04-15 23:38:17 +0700

    fixed index.blade

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -6,8 +6,6 @@
     <h1>Список статей</h1>
     @foreach ($articles as $article)
         <h2><a href="{{ route('articles.show', $article) }}">{{$article->name}}</a></h2>
-         {{-- Str::limit – функция-хелпер, которая обрезает текст до указанной длины --}}
-        {{-- Используется для очень длинных текстов, которые нужно сократить --}}
         <div>{{Str::limit($article->body, 200)}}</div>
         <a href="{{ route('articles.edit', $article) }}">Редактировать</a>
         <a href="{{ route('articles.destroy', $article) }}" data-confirm="Вы уверены?" class="text-danger"
